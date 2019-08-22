969f9d00ad97a4b7680f0e9a81b17d3fd88d1662
Joris <jorisreijbroek@gmail.com>
2019-08-22 22:54:32 +0200

    Update vartest.php

--- vartest.php
+++ vartest.php
@@ -1,9 +1,11 @@
 <?php
 
-$configVars = json_decode(file_get_contents("/apps/snor-bot/config-vars"));
+/*$configVars = json_decode(file_get_contents("/apps/snor-bot/config-vars"));
 
 echo"<pre>";
 print_r($configVars);
-echo"</pre>";
+echo"</pre>";*/
+
+echo getenv('telegramId');
 
 ?>
