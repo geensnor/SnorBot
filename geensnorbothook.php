e2eefdfc1d36d8c8f028e4f3ef15614748448dff
Joris <jorisreijbroek@gmail.com>
2019-11-16 10:05:04 +0100

    Update geensnorbothook.php

--- geensnorbothook.php
+++ geensnorbothook.php
@@ -38,7 +38,14 @@ $send = FALSE;
 
 	if($text == 'temperatuur nijmegen') {
 		$weerObject = json_decode(file_get_contents("https://api.darksky.net/forecast/".getenv('DarkskyToken')."/51.827359,5.853042?lang=nl&units=ca"));
-		$content = array('chat_id' => $chat_id, 'text' => "In Nijmegen is het nu ".$weerObject->daily->temperature." graden");
+		$content = array('chat_id' => $chat_id, 'text' => "In Nijmegen is het nu ".$weerObject->currently->temperature." graden");
+		$telegram->sendMessage($content);
+		$send = TRUE;
+	}
+
+	if($text == 'temperatuur utrecht') {
+		$weerObject = json_decode(file_get_contents("https://api.darksky.net/forecast/".getenv('DarkskyToken')."/52.092921,5.123173?lang=nl&units=ca"));
+		$content = array('chat_id' => $chat_id, 'text' => "In Nijmegen is het nu ".$weerObject->currently->temperature." graden");
 		$telegram->sendMessage($content);
 		$send = TRUE;
 	}
