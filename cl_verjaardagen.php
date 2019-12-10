00c3abae5c19032470f847c93586b3659d55c4c1
Joris <jorisreijbroek@gmail.com>
2019-12-10 09:11:05 +0100

    Bug opgelost
    Op de dag van de verjaardag werd iemand 1 jaar te oud.

--- cl_verjaardagen.php
+++ cl_verjaardagen.php
@@ -63,7 +63,7 @@ class verjaardag{
     function getVerjaardagTekst(){
         $verjaardagenData = $this->getVerjaardagenData();
         if($verjaardagenData[0]["dagenTotVerjaardag"] == 0)
-            return "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijd"] + 1)." jaar oud!";
+            return "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijd"])." jaar oud!";
         else
             return $verjaardagenData[0]["naam"]." is de volgende die jarig is. Hij/zij wordt over ".$verjaardagenData[0]["dagenTotVerjaardag"]." dagen (".$verjaardagenData[0]["datumVerjaardag"].") ".($verjaardagenData[0]["leeftijd"] + 1)." jaar.";
     }
