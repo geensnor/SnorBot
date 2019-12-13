927198fe14febf552538db10b84cc47a9b575140
Joris <jorisreijbroek@gmail.com>
2019-12-13 21:46:53 +0100

    Update cl_verjaardagen.php

--- cl_verjaardagen.php
+++ cl_verjaardagen.php
@@ -35,19 +35,19 @@ class verjaardag{
 
         	$persoon["naam"] = $this->geboortedatums[$key]->naam;
         	$persoon["geboortedatum"] = date('d-m-Y', strtotime($this->geboortedatums[$key]->geboortedatum));
-        	$persoon["leeftijd"] = floor(($referentieDatum - strtotime($this->geboortedatums[$key]->geboortedatum))/31557600);
-            if($verschil >= 0){
-            	
+                $leeftijdObject = date_diff(date_create($this->geboortedatums[$key]->geboortedatum), date_create(date('Y-m-d')));
+            $persoon["leeftijdJaren"] = $leeftijdObject->y;
+            $persoon["leeftijdDagen"] = $leeftijdObject->days;
+
+            if($verschil >= 0){           	
             	$persoon["dagenTotVerjaardag"] = round($verschil/86400);
             	$persoon["datumVerjaardag"] = date('d-m-Y', $verjaardagDitJaar);
-                
             } 
             else{//Verjaardag is al geweest dit jaar
             	$verjaardagVolgendJaar = strtotime(date("Y",strtotime("+1 year")).date("-m-d", strtotime($this->geboortedatums[$key]->geboortedatum)));
             	$verschil = $verjaardagVolgendJaar  - $referentieDatum;
             	$persoon["dagenTotVerjaardag"] = round($verschil/86400);
             	$persoon["datumVerjaardag"] = date('d-m-Y', $verjaardagVolgendJaar);
-
             }
             $verjaardagenData[] = $persoon;
         	
@@ -63,17 +63,17 @@ class verjaardag{
     function getVerjaardagTekst(){
         $verjaardagenData = $this->getVerjaardagenData();
         if($verjaardagenData[0]["dagenTotVerjaardag"] == 0)
-            return "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijd"])." jaar oud!";
+            return "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijdJaren"])." jaar oud!";
         else
-            return $verjaardagenData[0]["naam"]." is de volgende die jarig is. Hij/zij wordt over ".$verjaardagenData[0]["dagenTotVerjaardag"]." dagen (".$verjaardagenData[0]["datumVerjaardag"].") ".($verjaardagenData[0]["leeftijd"] + 1)." jaar.";
+            return $verjaardagenData[0]["naam"]." is de volgende die jarig is. Hij/zij wordt over ".$verjaardagenData[0]["dagenTotVerjaardag"]." dagen (".$verjaardagenData[0]["datumVerjaardag"].") ".($verjaardagenData[0]["leeftijdJaren"] + 1)." jaar. ";
     }
 
     function checkKomendeDagen(){
         $verjaardagenData = $this->getVerjaardagenData();
         if($verjaardagenData[0]["dagenTotVerjaardag"] == 0)
-            $returnString = "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijd"])." jaar oud!";
+            $returnString = "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijdJaren"])." jaar oud!";
         if($verjaardagenData[0]["dagenTotVerjaardag"] == 1)
-            $returnString = "Morgen wordt ".$verjaardagenData[0]["naam"]." al weer ".($verjaardagenData[0]["leeftijd"] + 1)." jaar oud!";
+            $returnString = "Morgen wordt ".$verjaardagenData[0]["naam"]." al weer ".($verjaardagenData[0]["leeftijdJaren"] + 1)." jaar oud!";
         
         return $returnString;
     }
