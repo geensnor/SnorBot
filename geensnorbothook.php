da9e7377229f611cbbce75e265ad3c79b2c84ba3
ErikvZ <erik@netninja.nl>
2021-08-15 22:23:56 +0200

    Update geensnorbothook.php

--- geensnorbothook.php
+++ geensnorbothook.php
@@ -67,7 +67,7 @@ function getNews() {
 function getCyclingNews() {
 	$nuxml = simplexml_load_file("http://feeds.nos.nl/nossportwielrennen");
 	
-	return "Laatste nieuws van nos.nl: \n[".$nuxml->channel->item[0]->title."](".$nuxml->channel->item[0]->link.")";
+	return "Laatste wielrennieuws van nos.nl: \n[".$nuxml->channel->item[0]->title."](".$nuxml->channel->item[0]->link.")";
 }
 
 function getHackerNews() {
