a4c93d380ce3e58ec159f06a33b139893e0ad14f
Joris <jorisreijbroek@gmail.com>
2021-11-24 22:37:08 +0100

    tournaam in bericht

--- tourpoule.php
+++ tourpoule.php
@@ -1,6 +1,6 @@
 <?php
 
-function getTourRanking($tourLocation)
+function getTourRanking($tourLocation, $tourName)
 {
     $ranking = json_decode(file_get_contents("https://www.geensnor.nl/tourpoule/rankingData/2021/tour-du-test/totalRanking.json"));
 
@@ -8,7 +8,7 @@ function getTourRanking($tourLocation)
         $rankingReturn .= $rank->userName.": ".$rank->points." \n";
     }
 
-    return "# Klassement Geensnor Tourpoule \n\n".$rankingReturn."\n\n[https://www.geensnor.nl/tourpoule/](https://www.geensnor.nl/tourpoule/)";
+    return "Klassement Geensnor Tourpoule: ".$tourName." \n\n".$rankingReturn."\n\n[https://www.geensnor.nl/tourpoule/](https://www.geensnor.nl/tourpoule/)";
 }
 
 function getTourInfo()
@@ -20,7 +20,7 @@ function getTourInfo()
         $returnText = "De volgende tour is ".$tourConfig->name." die op ".$tourConfig->start." start. Zodra er meer renners bekend zijn, kun je hier je eigen ploeg samenstellen!";
     } else {//Tour is geopend.
         if (strtotime($tourConfig->start) < time()) {
-            $returnText = getTourRanking($currentTourLocationJSON->currentTourLocation);
+            $returnText = getTourRanking($currentTourLocationJSON->currentTourLocation, $tourConfig->name);
         } else {
             $returnText = "Je kan je team voor ".$tourConfig->name." samenstellen op https://www.geensnor.nl/tourpoule";
         }
