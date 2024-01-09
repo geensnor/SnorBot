d23d51ac75a91b6e7f1c5e1435ac5850300e98bf
Joris <jorisreijbroek@gmail.com>
2024-01-09 21:49:11 +0100

    config voorbeeld

---
+++ config_example.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Hernoem dit bestand naar config.php en vervangen de XXX-en hieronder door je keys/id's/ditjes/datjes
+*/
+
+putenv("meteoserverKey=XXX");
+putenv("telegramId=XXX");
+putenv("verjaardagenGroupId=XXX");
+putenv("environment=Test");
+putenv("githubToken=XXX");
+
+?>
+
