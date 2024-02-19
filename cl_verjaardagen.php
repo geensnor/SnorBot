28eda375c8a3625fe930873e30d99c230347bf57
Joris <jorisreijbroek@gmail.com>
2024-02-19 17:07:49 +0000

    mooi maken

--- cl_verjaardagen.php
+++ cl_verjaardagen.php
@@ -5,17 +5,17 @@ class verjaardag
 
     public function __construct()
     {
-        $GithHubAPIUrl = "https://api.github.com/repos/reithose/geboortedatums/contents/";
+        $GithHubAPIUrl = 'https://api.github.com/repos/reithose/geboortedatums/contents/';
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($curl, CURLOPT_USERPWD, "reithose:".getenv('githubToken'));
-        curl_setopt($curl, CURLOPT_USERAGENT, "User-Agent: reithose");
+        curl_setopt($curl, CURLOPT_USERPWD, 'reithose:'.getenv('githubToken'));
+        curl_setopt($curl, CURLOPT_USERAGENT, 'User-Agent: reithose');
         curl_setopt($curl, CURLOPT_URL, $GithHubAPIUrl);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-        if (!curl_exec($curl)) {
-            die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+        if (! curl_exec($curl)) {
+            exit('Error: "'.curl_error($curl).'" - Code: '.curl_errno($curl));
         } else {
             $curlResult = json_decode(curl_exec($curl));
         }
@@ -30,30 +30,28 @@ class verjaardag
         $referentieDatum = strtotime(date('Y-m-d'));
 
         foreach ($this->geboortedatums as $key => $value) {
-
-            $verjaardagDitJaar = strtotime(date("Y").date("-m-d", strtotime($this->geboortedatums[$key]->geboortedatum)));
+            $verjaardagDitJaar = strtotime(date('Y').date('-m-d', strtotime($this->geboortedatums[$key]->geboortedatum)));
             $verschil = $verjaardagDitJaar - $referentieDatum;
 
-            $persoon["naam"] = $this->geboortedatums[$key]->naam;
-            $persoon["geboortedatum"] = date('d-m-Y', strtotime($this->geboortedatums[$key]->geboortedatum));
+            $persoon['naam'] = $this->geboortedatums[$key]->naam;
+            $persoon['geboortedatum'] = date('d-m-Y', strtotime($this->geboortedatums[$key]->geboortedatum));
             $leeftijdObject = date_diff(date_create($this->geboortedatums[$key]->geboortedatum), date_create(date('Y-m-d')));
-            $persoon["leeftijdJaren"] = $leeftijdObject->y;
-            $persoon["leeftijdDagen"] = $leeftijdObject->days;
+            $persoon['leeftijdJaren'] = $leeftijdObject->y;
+            $persoon['leeftijdDagen'] = $leeftijdObject->days;
 
-            if($verschil >= 0) {
-                $persoon["dagenTotVerjaardag"] = round($verschil / 86400);
-                $persoon["datumVerjaardag"] = date('d-m-Y', $verjaardagDitJaar);
+            if ($verschil >= 0) {
+                $persoon['dagenTotVerjaardag'] = round($verschil / 86400);
+                $persoon['datumVerjaardag'] = date('d-m-Y', $verjaardagDitJaar);
             } else {//Verjaardag is al geweest dit jaar
-                $verjaardagVolgendJaar = strtotime(date("Y", strtotime("+1 year")).date("-m-d", strtotime($this->geboortedatums[$key]->geboortedatum)));
-                $verschil = $verjaardagVolgendJaar  - $referentieDatum;
-                $persoon["dagenTotVerjaardag"] = round($verschil / 86400);
-                $persoon["datumVerjaardag"] = date('d-m-Y', $verjaardagVolgendJaar);
+                $verjaardagVolgendJaar = strtotime(date('Y', strtotime('+1 year')).date('-m-d', strtotime($this->geboortedatums[$key]->geboortedatum)));
+                $verschil = $verjaardagVolgendJaar - $referentieDatum;
+                $persoon['dagenTotVerjaardag'] = round($verschil / 86400);
+                $persoon['datumVerjaardag'] = date('d-m-Y', $verjaardagVolgendJaar);
             }
             $verjaardagenData[] = $persoon;
-
         }
 
-        if($verjaardagenData) {// usort geeft vervelende error als er geen data opgehaald kan worden.
+        if ($verjaardagenData) {// usort geeft vervelende error als er geen data opgehaald kan worden.
             usort($verjaardagenData, function ($a, $b) {
                 return $a['dagenTotVerjaardag'] - $b['dagenTotVerjaardag'];
             });
@@ -65,30 +63,28 @@ class verjaardag
     public function getVerjaardagTekst()
     {
         $verjaardagenData = $this->getVerjaardagenData();
-        if($verjaardagenData[0]["dagenTotVerjaardag"] == 0) {
-            return "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijdJaren"])." jaar oud!";
-        } elseif($verjaardagenData[0]["dagenTotVerjaardag"] == 1) {
-            return $verjaardagenData[0]["naam"]." is de volgende die jarig is. Hij/zij wordt morgen (".$verjaardagenData[0]["datumVerjaardag"].") ".($verjaardagenData[0]["leeftijdJaren"] + 1)." jaar!";
+        if ($verjaardagenData[0]['dagenTotVerjaardag'] == 0) {
+            return 'Hoera! '.$verjaardagenData[0]['naam'].' wordt vandaag '.($verjaardagenData[0]['leeftijdJaren']).' jaar oud!';
+        } elseif ($verjaardagenData[0]['dagenTotVerjaardag'] == 1) {
+            return $verjaardagenData[0]['naam'].' is de volgende die jarig is. Hij/zij wordt morgen ('.$verjaardagenData[0]['datumVerjaardag'].') '.($verjaardagenData[0]['leeftijdJaren'] + 1).' jaar!';
         } else {
-            return $verjaardagenData[0]["naam"]." is de volgende die jarig is. Hij/zij wordt over ".$verjaardagenData[0]["dagenTotVerjaardag"]." dagen (".$verjaardagenData[0]["datumVerjaardag"].") ".($verjaardagenData[0]["leeftijdJaren"] + 1)." jaar. ";
+            return $verjaardagenData[0]['naam'].' is de volgende die jarig is. Hij/zij wordt over '.$verjaardagenData[0]['dagenTotVerjaardag'].' dagen ('.$verjaardagenData[0]['datumVerjaardag'].') '.($verjaardagenData[0]['leeftijdJaren'] + 1).' jaar. ';
         }
     }
 
     public function checkKomendeDagen()
     {
         $verjaardagenData = $this->getVerjaardagenData();
-        if($verjaardagenData[0]["dagenTotVerjaardag"] == 0) {
-            $returnString = "Hoera! ".$verjaardagenData[0]["naam"]." wordt vandaag ".($verjaardagenData[0]["leeftijdJaren"])." jaar oud!";
+        if ($verjaardagenData[0]['dagenTotVerjaardag'] == 0) {
+            $returnString = 'Hoera! '.$verjaardagenData[0]['naam'].' wordt vandaag '.($verjaardagenData[0]['leeftijdJaren']).' jaar oud!';
         }
-        if($verjaardagenData[0]["dagenTotVerjaardag"] == 1) {
-            $returnString = "Morgen wordt ".$verjaardagenData[0]["naam"]." al weer ".($verjaardagenData[0]["leeftijdJaren"] + 1)." jaar oud!";
+        if ($verjaardagenData[0]['dagenTotVerjaardag'] == 1) {
+            $returnString = 'Morgen wordt '.$verjaardagenData[0]['naam'].' al weer '.($verjaardagenData[0]['leeftijdJaren'] + 1).' jaar oud!';
         }
 
         return $returnString;
     }
 }
-
-
 
 ?>
 
--- prijzenparade.php
+++ prijzenparade.php
@@ -8,7 +8,8 @@ function isToday($date_str)
     $match_date = new DateTime($date_str);
     $match_date->setTime(0, 0, 0);
     $diff = $today->diff($match_date);
-    $diff_days = (int)$diff->format("%R%a");
+    $diff_days = (int) $diff->format('%R%a');
+
     return $diff_days === 0;
 }
 
@@ -19,7 +20,7 @@ function get_prijzen_parade_url()
     foreach ($rss->channel->item as $item) {
         $title = $item->title;
         $publication_date = $item->pubDate;
-        if (strpos($title, ".Actie - December Prijzen") === 0 && isToday($publication_date)) {
+        if (strpos($title, '.Actie - December Prijzen') === 0 && isToday($publication_date)) {
             return $item->link;
         }
     }
