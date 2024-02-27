d24371931669a4cfd4467ec37ad4154838812e52
Joris <jorisreijbroek@gmail.com>
2024-02-27 20:34:59 +0100

    melding als er geen kalender opgehaald kan worden

--- wielrennen.php
+++ wielrennen.php
@@ -6,50 +6,55 @@ include __DIR__.'/vendor/autoload.php';
 
 function getKoersenTekst(array $parsedICS, string $referentieDatum): string
 {
-    $koersenTekst = '';
-    $koersenTekstBinnenkort = '';
-    foreach ($parsedICS as $koers) {
-        //Actuele koersen
-        if ($koers->dtstart == $referentieDatum && $koers->dtend == $referentieDatum + 1) {//Eendaagse koers, vandaag
-            $koersenTekst .= ' \n Vandaag wordt '.$koers->summary.' gereden.';
-        } elseif ($koers->dtstart <= $referentieDatum && $koers->dtend - 1 >= $referentieDatum) {//Meerdaagse koers, vandaag bezig
 
-            if ($koers->dtstart == $referentieDatum) {//Meerdaagse koers, en de start is vandaag
-                $koersenTekst .= ' \n Vandaag start '.$koers->summary.'. Deze duurt tot en met '.getFormattedDate(strtotime('yesterday', strtotime($koers->dtend)));
-
-            } elseif ($koers->dtend - 1 == $referentieDatum) {
-
-                $koersenTekst .= '\n Vandaag is de finish van '.$koers->summary.'.';
-
-            } else {//Meerdaagse koers en hij is eerder gestart
-                $dagVanKoers = $referentieDatum - $koers->dtstart + 1;
-                $koersenTekst .= "\n Vandaag is dag ".$dagVanKoers.' van '.$koers->summary.'. Deze duurt tot en met '.getFormattedDate(strtotime('yesterday', strtotime($koers->dtend))).'.';
-            }
-
-        }
-
-        //Binnenkort
-        if (strtotime($koers->dtstart) > strtotime($referentieDatum) && strtotime($koers->dtstart) < strtotime('+2 week', strtotime($referentieDatum))) {
-
-            $koersenTekstBinnenkort .= "\n- ".getFormattedDate(strtotime($koers->dtstart)).' start '.$koers->summary.'.';
-            if ((strtotime($koers->dtend) - strtotime($koers->dtstart)) > 86400) {
-
-                $koersenTekstBinnenkort .= ' Deze duurt tot en met '.getFormattedDate(strtotime('yesterday', strtotime($koers->dtend))).'.';
-            }
-        }
-    }
-
-    if ($koersenTekst) {
-        $koersenTekst = "**Het is koers!** \n".$koersenTekst;
+    if (! isset($parsedICS[0])) {
+        return 'Kan geen wielrenkalender ophalen';
     } else {
-        $koersenTekst = "Er wordt vandaag niet gekoerst.\n";
-    }
+        $koersenTekst = '';
+        $koersenTekstBinnenkort = '';
+        foreach ($parsedICS as $koers) {
+            //Actuele koersen
+            if ($koers->dtstart == $referentieDatum && $koers->dtend == $referentieDatum + 1) {//Eendaagse koers, vandaag
+                $koersenTekst .= ' \n Vandaag wordt '.$koers->summary.' gereden.';
+            } elseif ($koers->dtstart <= $referentieDatum && $koers->dtend - 1 >= $referentieDatum) {//Meerdaagse koers, vandaag bezig
 
-    if ($koersenTekstBinnenkort) {
-        $koersenTekst .= "\nBinnenkort:".$koersenTekstBinnenkort;
-    }
+                if ($koers->dtstart == $referentieDatum) {//Meerdaagse koers, en de start is vandaag
+                    $koersenTekst .= ' \n Vandaag start '.$koers->summary.'. Deze duurt tot en met '.getFormattedDate(strtotime('yesterday', strtotime($koers->dtend)));
 
-    return $koersenTekst;
+                } elseif ($koers->dtend - 1 == $referentieDatum) {
+
+                    $koersenTekst .= '\n Vandaag is de finish van '.$koers->summary.'.';
+
+                } else {//Meerdaagse koers en hij is eerder gestart
+                    $dagVanKoers = $referentieDatum - $koers->dtstart + 1;
+                    $koersenTekst .= "\n Vandaag is dag ".$dagVanKoers.' van '.$koers->summary.'. Deze duurt tot en met '.getFormattedDate(strtotime('yesterday', strtotime($koers->dtend))).'.';
+                }
+
+            }
+
+            //Binnenkort
+            if (strtotime($koers->dtstart) > strtotime($referentieDatum) && strtotime($koers->dtstart) < strtotime('+2 week', strtotime($referentieDatum))) {
+
+                $koersenTekstBinnenkort .= "\n- ".getFormattedDate(strtotime($koers->dtstart)).' start '.$koers->summary.'.';
+                if ((strtotime($koers->dtend) - strtotime($koers->dtstart)) > 86400) {
+
+                    $koersenTekstBinnenkort .= ' Deze duurt tot en met '.getFormattedDate(strtotime('yesterday', strtotime($koers->dtend))).'.';
+                }
+            }
+        }
+
+        if ($koersenTekst) {
+            $koersenTekst = "**Het is koers!** \n".$koersenTekst;
+        } else {
+            $koersenTekst = "Er wordt vandaag niet gekoerst.\n";
+        }
+
+        if ($koersenTekstBinnenkort) {
+            $koersenTekst .= "\nBinnenkort:".$koersenTekstBinnenkort;
+        }
+
+        return $koersenTekst;
+    }
 }
 
 function getCyclingNews()
