e362facf44158b55aa17f3c7d0a6b1ef3918fe57
Erik v Z <erik@netninja.nl>
2024-03-05 13:58:56 +0100

    Update WielrennenTest.php

--- tests/WielrennenTest.php
+++ tests/WielrennenTest.php
@@ -22,6 +22,5 @@ test('Er wordt een koers gereden op de gekozen datum.', function () {
 
 test('Er start morgen een koers', function () {
     $parsedICS = json_decode(file_get_contents('tests/fixtures/cyclingCalendar2024.json'));
-    expect(getKoersenTekst($parsedICS, '20240301'))->
-    toContain('morgen');
+    expect(getKoersenTekst($parsedICS, '20240301'))->toContain('morgen');
 });
