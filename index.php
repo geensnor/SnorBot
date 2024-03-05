22a8830e965b9ca3b2699710bd202eaa66b9a2fb
Zuijdam <Zuijdam@users.noreply.github.com>
2024-03-05 11:09:58 +0000

    Apply Laravel Pint changes

--- index.php
+++ index.php
@@ -3,4 +3,3 @@
 include 'config.php';
 
 echo "SnorBot!<br><br>Voor meer informatie:<br><a href='https://github.com/geensnor/SnorBot'>https://github.com/geensnor/SnorBot</a><br><br>".getenv('environment');
-?>
