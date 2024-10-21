895620fe3afcdd6c6cb72e8b24630d0cc0e737c9
Joris <jorisreijbroek@gmail.com>
2024-10-21 18:20:34 +0200

    dag van de fix

--- functies.php
+++ functies.php
@@ -73,11 +73,15 @@ function getEthereumPrice(): string
 
 function getDagVanDe()
 {
-    $dagVanDeLocatie = 'https://raw.githubusercontent.com/geensnor/DigitaleTuin/master/_data/dagvande.json';
+    $dagVanDeLocatie = 'https://raw.githubusercontent.com/geensnor/DeDigitaleTuin/refs/heads/main/src/content/data/dagvande.json';
+
     $dagVanDeArray = json_decode(file_get_contents($dagVanDeLocatie));
+
     foreach ($dagVanDeArray as $key => $value) {
+        echo "<br>lala:".$dagVanDeArray[$key]->dag;
         if ($dagVanDeArray[$key]->dag == date('d-m')) {
             $dagText = 'Het is vandaag '.$dagVanDeArray[$key]->onderwerp;
+
         }
     }
 
