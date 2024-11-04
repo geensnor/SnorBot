f40267173135e9dfaccb730ee3f15dc8da0e3d8d
Joris <jorisreijbroek@gmail.com>
2024-11-04 16:35:12 +0100

    foutafhandeling

--- cl_TweedeKamer.php
+++ cl_TweedeKamer.php
@@ -8,7 +8,7 @@ class TweedeKamer
         //TODO: alles
     }
 
-    public function getGeschenk(): object
+    public function getGeschenk(): object|string
     {
 
         $arrContextOptions = [
@@ -18,26 +18,25 @@ class TweedeKamer
             ],
         ];
 
-        try {
+        $geschenkResponseContent = file_get_contents('https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/PersoonGeschenk?&$expand=%20Persoon&$orderby=Datum%20desc', false, stream_context_create($arrContextOptions));
 
-            $geschenkResponse = json_decode(file_get_contents('https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/PersoonGeschenk?&$expand=%20Persoon&$orderby=Datum%20desc', false, stream_context_create($arrContextOptions)));
+        if ($geschenkResponseContent === false) {
+            return 'Ophalen van de geschenken gaat niet helemaal goed.';
+        } else {
 
-        } catch (Exception $e) {
+            $geschenkResponse = json_decode($geschenkResponseContent);
 
+            $geschenkObject = new stdClass();
+            $geschenkObject->tekst = $geschenkResponse->value[0]->Omschrijving;
+            $geschenkObject->naam = $geschenkResponse->value[0]->Persoon->Roepnaam.($geschenkResponse->value[0]->Persoon->Tussenvoegsel ? ' '.$geschenkResponse->value[0]->Persoon->Tussenvoegsel.' ' : ' ').$geschenkResponse->value[0]->Persoon->Achternaam;
+            $geschenkObject->nummer = $geschenkResponse->value[0]->Persoon->Nummer;
+            $geschenkObject->woonplaats = $geschenkResponse->value[0]->Persoon->Woonplaats;
+            $datumObject = new DateTime('2024-10-24T00:00:00+02:00');
+
+            $geschenkObject->datum = getFormattedDate(new DateTime($geschenkResponse->value[0]->Datum));
+
+            return $geschenkObject;
         }
-
-        $geschenkResponse = json_decode(file_get_contents('https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/PersoonGeschenk?&$expand=%20Persoon&$orderby=Datum%20desc', false, stream_context_create($arrContextOptions)));
-
-        $geschenkObject = new stdClass();
-        $geschenkObject->tekst = $geschenkResponse->value[0]->Omschrijving;
-        $geschenkObject->naam = $geschenkResponse->value[0]->Persoon->Roepnaam.($geschenkResponse->value[0]->Persoon->Tussenvoegsel ? ' '.$geschenkResponse->value[0]->Persoon->Tussenvoegsel.' ' : ' ').$geschenkResponse->value[0]->Persoon->Achternaam;
-        $geschenkObject->nummer = $geschenkResponse->value[0]->Persoon->Nummer;
-        $geschenkObject->woonplaats = $geschenkResponse->value[0]->Persoon->Woonplaats;
-        $datumObject = new DateTime('2024-10-24T00:00:00+02:00');
-
-        $geschenkObject->datum = getFormattedDate(new DateTime($geschenkResponse->value[0]->Datum));
-
-        return $geschenkObject;
     }
 
     public function getActiviteitTekst()
@@ -48,7 +47,11 @@ class TweedeKamer
     public function getGeschenkTekst(): string
     {
         $geschenkObject = $this->getGeschenk();
+        if (is_object($geschenkObject)) {
 
-        return 'Het laatste geschenk voor kamerleden is van '.$geschenkObject->datum.'. ['.$geschenkObject->naam.'](https://berthub.eu/tkconv/persoon.html?nummer='.$geschenkObject->nummer.') uit '.$geschenkObject->woonplaats.' kreeg: '.$geschenkObject->tekst;
+            return 'Het laatste geschenk voor kamerleden is van '.$geschenkObject->datum.'. ['.$geschenkObject->naam.'](https://berthub.eu/tkconv/persoon.html?nummer='.$geschenkObject->nummer.') uit '.$geschenkObject->woonplaats.' kreeg: '.$geschenkObject->tekst;
+        } else {
+            return $geschenkObject;
+        }
     }
 }
