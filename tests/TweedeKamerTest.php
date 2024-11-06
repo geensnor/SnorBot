771c46180d9bdd2f72662ddd201c50bb02d9ece7
Joris <jorisreijbroek@gmail.com>
2024-11-06 13:37:11 +0100

    fix test

--- tests/TweedeKamerTest.php
+++ tests/TweedeKamerTest.php
@@ -22,5 +22,5 @@ test('Activiteiten op een maandag, met activiteiten', function () {
     $tk = new TweedeKamer();
     $tijd = new DateTime('2024-11-04T16:00:00+01:00'); //Is een maandag
     $activiteitTekst = $tk->getActiviteitTekst($tijd);
-    expect($activiteitTekst)->toContain('gebeurt weer van alles in de Tweede Kamer. Zo is');
+    expect($activiteitTekst)->toContain('alles in de Tweede Kamer. Zo is er om');
 });
