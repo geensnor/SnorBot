598bbe53ea37023c0c285edb0bb0d356405fe83d
Joris <jorisreijbroek@gmail.com>
2024-11-08 09:56:41 +0100

    Weekenden test (#142)
    * geschenken tweede kamer
    
    * details....
    
    * Net iets mooier tweaken..
    
    * dammit..
    
    * zucht
    
    * wat een drama dit
    
    * hopelijk zien m'n kinderen dit niet...
    
    * bijna..
    
    * probeer het anders eerst even lokaal..
    
    * dan maar zo.
    
    * betere zinsbouw
    
    * Apply Laravel Pint changes
    
    * foutafhandeling
    
    * tweede kamer activiteiten
    
    * Apply Laravel Pint changes
    
    * vandaag
    
    * juist methode
    
    * tweede kamer in goede morgen
    
    * vandaag verhuisd
    
    * Apply Laravel Pint changes
    
    * fix test
    
    * } vergeten
    
    * Apply Laravel Pint changes
    
    * weekenden test
    
    ---------
    
    Co-authored-by: reithose <reithose@users.noreply.github.com>

---
+++ tests/WeekendenTest.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'cl_weekenden.php';
+
+test('Weekendtekst voor tekst met jaartal', function (): void {
+
+    $weekend = new weekend();
+    $weekendenJSON = json_decode(file_get_contents('tests/fixtures/weekenden.json'));
+    $weekend->setWeekenden($weekendenJSON);
+    $weekendTekst = $weekend->getWeekendText('Wat was er in 2015?');
+    expect($weekendTekst)->toBe('In 2015 gingen we naar Rotterdam: Havenstad');
+});
+
+test('Weekendtekst voor tekst zonder jaartal', function (): void {
+
+    $weekend = new weekend();
+    $weekendenJSON = json_decode(file_get_contents('tests/fixtures/weekenden.json'));
+    $weekend->setWeekenden($weekendenJSON);
+    $weekendTekst = $weekend->getWeekendText('Wat was er zonder jaartal?');
+    expect($weekendTekst)->toBe('Dit is geen jaartal');
+});
+
+test('Weekendtekst tekst met jaartal die niet voorkomt', function (): void {
+
+    $weekend = new weekend();
+    $weekendenJSON = json_decode(file_get_contents('tests/fixtures/weekenden.json'));
+    $weekend->setWeekenden($weekendenJSON);
+    $weekendTekst = $weekend->getWeekendText('Wat was er in 2115?');
+    expect($weekendTekst)->toBe('In 2115 gingen we geen weekend weg');
+});
