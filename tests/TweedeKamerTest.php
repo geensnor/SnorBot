9bfb41c98e26bab436fade509be5e928a7e98283
Joris <jorisreijbroek@gmail.com>
2024-11-18 09:38:02 +0100

    Fix dubbele testen (#145)
    * thuisarts
    
    thuisarts
    
    * fix
    
    * Apply Laravel Pint changes
    
    * Apply Laravel Pint changes
    
    * dubbele test eruit
    
    * Apply Laravel Pint changes
    
    ---------
    
    Co-authored-by: Erich <35580176+Zuijdam@users.noreply.github.com>
    Co-authored-by: Zuijdam <Zuijdam@users.noreply.github.com>
    Co-authored-by: reithose <reithose@users.noreply.github.com>

--- tests/TweedeKamerTest.php
+++ tests/TweedeKamerTest.php
@@ -14,22 +14,7 @@ test('Activiteitentekst op een zondag, zonder activiteiten', function () {
     $tk = new TweedeKamer;
     $tijd = new DateTime('2024-11-03T16:00:00+01:00'); //Is een zondag
     $activiteitTekst = $tk->getActiviteitTekst($tijd);
-    expect($activiteitTekst)->toBe('Er is vandaag niet veel te doen in de Tweede Kamer');
-});
-
-test('Activiteiten op een maandag, met activiteiten', function () {
-    $tk = new TweedeKamer;
-    $tijd = new DateTime('2024-11-04T16:00:00+01:00'); //Is een maandag
-    $activiteitTekst = $tk->getActiviteitTekst($tijd);
-    expect($activiteitTekst)->toContain('alles in de Tweede Kamer. Zo is er om');
-});
-
-test('Activiteitentekst op een zondag, zonder activiteiten', function () {
-    $tk = new TweedeKamer;
-    $tijd = new DateTime('2024-11-03T16:00:00+01:00'); //Is een zondag
-    $activiteitTekst = $tk->getActiviteitTekst($tijd);
-    expect($activiteitTekst)->toBe('Er is vandaag niet veel te doen in de Tweede Kamer');
-
+    expect($activiteitTekst)->toContain('Er is vandaag niet veel te doen in de Tweede Kamer');
 });
 
 test('Activiteiten op een maandag, met activiteiten', function () {
