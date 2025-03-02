b53f4ee44feb63d1e1e2b158cbb2d90bd5b1d843
Joris <jorisreijbroek@gmail.com>
2025-03-02 18:20:37 +0100

    dag test

--- tests/EnergieTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'config.php';
-require 'energie.php';
-
-test('Er wordt nog energie gebruikt in Nederland', function (): void {
-    $energieObject = getEnergie();
-    expect($energieObject->powerConsumptionTotal)->not->toBeNull();
-});
