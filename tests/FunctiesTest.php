40407d3fe84b284e5cb8237a85a559a3722e3edd
Joris <jorisreijbroek@gmail.com>
2025-06-01 21:40:33 +0200

    ben nog niet kapot van de yaml parser...

--- tests/FunctiesTest.php
+++ tests/FunctiesTest.php
@@ -11,3 +11,9 @@ test('Vandaag van wikipedia', function (): void {
     expect($event->year)->toBeString();
     expect($event->content)->toBeString();
 });
+
+test('YAML parser', function (): void {
+    $yamlString = file_get_contents("tests/fixtures/test.yaml");
+    $outputParseYaml = parseYaml($yamlString);
+    expect($outputParseYaml[0]->jaar)->toBe("2016");
+});
