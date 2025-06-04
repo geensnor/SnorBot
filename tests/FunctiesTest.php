84507cbc10ffc037bc010ed8bfdb3ad740063f81
Joris <jorisreijbroek@gmail.com>
2025-06-04 23:16:38 +0200

    testen

--- tests/FunctiesTest.php
+++ tests/FunctiesTest.php
@@ -11,3 +11,18 @@ test('Vandaag van wikipedia', function (): void {
     expect($event->year)->toBeString();
     expect($event->content)->toBeString();
 });
+
+test('Simpele lijsten laatste', function (): void {
+    $laatsteItemTestLijst = getItemSimpeleLijst("laatste", "tests/fixtures/simpeleLijstenLijst.json");
+    expect($laatsteItemTestLijst)->toBe("Simpel item 3");
+});
+
+test('Simpele lijsten willekeurig', function (): void {
+    $laatsteItemTestLijst = getItemSimpeleLijst("willekeurig", "tests/fixtures/simpeleLijstenLijst.json");
+    expect($laatsteItemTestLijst)->toBeString();
+});
+
+test('Simpele lijsten niet bestaande tekst', function (): void {
+    $laatsteItemTestLijst = getItemSimpeleLijst("niet bestaande tekst", "tests/fixtures/simpeleLijstenLijst.json");
+    expect($laatsteItemTestLijst)->toBeFalse();
+});
