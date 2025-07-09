6dacea1d1e8e4772948a58b8ee4952b26faf4e79
Joris <jorisreijbroek@gmail.com>
2025-07-09 23:11:42 +0200

    uitslag vandaag

--- tourpoule.php
+++ tourpoule.php
@@ -1,5 +1,46 @@
 <?php
 
+function getTodayRanking(): string
+{
+    $pouleResult = json_decode(file_get_contents('https://www.geensnor.nl/tourpoule/api/summaryToday/'));
+
+    $place = 1;
+    $rankingReturn = '';
+
+    // Verzamel alle namen en punten om de maximale lengte te bepalen
+    $maxNameLen = 0;
+    $maxPointsLen = 0;
+    foreach ($pouleResult->latestStageUserRanking as $todayRank) {
+        $nameLen = mb_strlen($todayRank->name);
+        $pointsLen = mb_strlen((string)$todayRank->points);
+        if ($nameLen > $maxNameLen) {
+            $maxNameLen = $nameLen;
+        }
+        if ($pointsLen > $maxPointsLen) {
+            $maxPointsLen = $pointsLen;
+        }
+    }
+
+    // Bouw de uitgelijnde lijst op
+    foreach ($pouleResult->latestStageUserRanking as $todayRank) {
+        $name = html_entity_decode($todayRank->name);
+        $points = $todayRank->points;
+        // Bereken het aantal punten dat nodig is om op te vullen
+        $dotsCount = ($maxNameLen - mb_strlen($name)) + 3; // 3 extra voor vaste spatie na naam
+        $dots = str_repeat('.', $dotsCount);
+        $rankingReturn .= sprintf(
+            "`%2d. %s%s %{$maxPointsLen}d`\n",
+            $place,
+            $name,
+            $dots,
+            $points
+        );
+        $place++;
+    }
+
+    return "*Uitslag etappe ".$pouleResult->lastUpdate->stageNumber.": ".$pouleResult->stageToday->route."*\n\n".$rankingReturn."\nZie de volledige uitslag op: [geensnor.nl/tourpoule/](https://www.geensnor.nl/tourpoule/)";
+}
+
 /**
  * Maakt een mooi overzicht van de Geensnor Tourpoule deelnemers
  *
