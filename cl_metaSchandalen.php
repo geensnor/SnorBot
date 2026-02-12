57e40d44d07b8a49ae5d5e9e55e84607f5bed47d
Joris <jorisreijbroek@gmail.com>
2026-02-12 20:51:48 +0100

    schandalen in de snorbot!

---
+++ cl_metaSchandalen.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Een klasse die de schandalen lijst bevat.
+ */
+
+class schandalenLijst
+{
+    private array $schandalen;
+
+    public function __construct($jsonLokatie)
+    {
+        $this->schandalen = json_decode(file_get_contents($jsonLokatie));
+    }
+
+    /**
+     * Factory maakt van een stdClass object een Schandaal object.
+     *
+     * @return schandaal
+     */
+    private function maakSchandaal(stdClass $data): schandaal
+    {
+        return new schandaal(
+            $data->datum,
+            $data->schandaal,
+            $data->bron,
+            $data->url,
+            $data->archief ?? null
+        );
+    }
+    /**
+     * Haalt een random schandaal op.
+     *
+     * @return schandaal|null Geeft een schandaal terug of null als er geen schandalen zijn
+     */
+    public function getWillekeurigSchandaal(): ?schandaal
+    {
+        if (empty($this->schandalen)) {
+            return null;
+        }
+
+        $willekeurigeIndex = array_rand($this->schandalen);
+        return $this->maakSchandaal($this->schandalen[$willekeurigeIndex]);
+    }
+
+    /**
+     * Haalt het laatste schandaal op.
+     *
+     * @return schandaal|null Geeft een schandaal terug of null als er geen schandalen zijn
+     */
+    public function getLaatsteSchandaal(): ?schandaal
+    {
+        if (empty($this->schandalen)) {
+            return null;
+        }
+        return $this->maakSchandaal($this->schandalen[0]);
+    }
+}
+
+/**
+ * Een klasse van een schandaal.
+ */
+class schandaal
+{
+    public function __construct(
+        string $datum,
+        string $schandaal,
+        string $bron,
+        string $url,
+        ?string $archief = null
+    ) {
+        $this->datum = $datum;
+        $this->schandaal = $schandaal;
+        $this->bron = $bron;
+        $this->url = $url;
+        $this->archief = $archief;
+    }
+    public readonly string $datum;
+    public readonly string $schandaal;
+    public readonly string $bron;
+    public readonly string $url;
+    public readonly ?string $archief;
+}
+
+/**
+ * Een klasse die de tekstversie van een schandaal bevat.
+ */
+
+class schandaalTekst
+{
+    public static function geefTekstRandom(schandaal $schandaal): string
+    {
+        return "In {$schandaal->datum} was er een schandaal:\n {$schandaal->schandaal}\n\n bron: {$schandaal->bron}";
+    }
+
+    public static function geefTekstLaatste(schandaal $schandaal): string
+    {
+        return "Het laastste Meta schandaal: \n {$schandaal->schandaal}\n\n bron: {$schandaal->bron}";
+    }
+
+}
